ef385f319a89904aecc2cca87327019118c25a04
Ryan Leadbetter <ryan.leadbetter@postgrad.curtin.edu.au>
2025-04-09 08:47:24 +1000

    Style mrPerformancePlot().

--- R/mrPerformancePlot.R
+++ R/mrPerformancePlot.R
@@ -12,106 +12,128 @@
 #' \item{wide_df}{A dataframe with the wide format of model performance metrics and their differences.}
 #' @export
 #' @examples
-#'plots <- mrPerformancePlot(ModelPerf1 =ModelPerf_lm, ModelPerf2 = ModelPerf_rf, mod_names=c('linear_reg','rand_forest'), mode='regression' )
-#' 
-mrPerformancePlot <- function(ModelPerf1 = NULL, ModelPerf2 = NULL,
-                              mod_names=c('combined','Xonly_model'), mode='classification' ) {
-  
+#' plots <- mrPerformancePlot(ModelPerf1 = ModelPerf_lm, ModelPerf2 = ModelPerf_rf, mod_names = c("linear_reg", "rand_forest"), mode = "regression")
+#'
+mrPerformancePlot <- function(ModelPerf1 = NULL,
+                              ModelPerf2 = NULL,
+                              mod_names = c("combined", "Xonly_model"),
+                              mode = "classification") {
   m1 <- ModelPerf1[[1]]
   m2 <- ModelPerf2[[1]]
-  
-  d_length <- nrow(m1) #has to be the same
-  
-  if (mode=='classification'){
-    
-    m1$mcc <- as.numeric(m1$mcc) #extract mcc
-    
+
+  d_length <- nrow(m1) # has to be the same
+
+  if (mode == "classification") {
+    m1$mcc <- as.numeric(m1$mcc) # extract mcc
+
     m1 <- m1 %>%
       dplyr::rename(metric = mcc)
-    
-    model1_df <- m1 %>% tibble(model_type = rep(mod_names[1], d_length)) %>% 
-      replace_na(list(metric = 0)) #NAs happen when there is no variance in the model predictions (i.e.all 0s), for these make NAs=0
-    
+
+    model1_df <- m1 %>%
+      tibble(model_type = rep(mod_names[1], d_length)) %>%
+      replace_na(list(metric = 0)) # NAs happen when there is no variance in the model predictions (i.e.all 0s), for these make NAs=0
+
     m2$mcc <- as.numeric(m2$mcc)
-    
-    model2_df <- m2 %>% tibble(model_type = rep(mod_names[2], d_length)) %>% 
-      dplyr::rename(metric=mcc) %>%
+
+    model2_df <- m2 %>%
+      tibble(model_type = rep(mod_names[2], d_length)) %>%
+      dplyr::rename(metric = mcc) %>%
       replace_na(list(metric = 0))
- 
   } else {
-    
-    model1_df <- m1 %>% tibble(model_type = rep(mod_names[1], d_length)) %>% 
-      dplyr::rename(metric=rmse)
-    
-    model2_df <- m2 %>% tibble(model_type = rep(mod_names[2], d_length)) %>% 
-      dplyr::rename(metric=rmse)
-    
+    model1_df <- m1 %>%
+      tibble(model_type = rep(mod_names[1], d_length)) %>%
+      dplyr::rename(metric = rmse)
+
+    model2_df <- m2 %>%
+      tibble(model_type = rep(mod_names[2], d_length)) %>%
+      dplyr::rename(metric = rmse)
   }
-    model_compare_df <- bind_rows(model1_df , model2_df )  
-  
-#detect outliers function
-findoutlier <- function(x) {
-  return(x < quantile(x, .25) - 1.5*IQR(x) | x > quantile(x, .75) + 1.5*IQR(x))
-}  
-  #create data frame
+  model_compare_df <- bind_rows(model1_df, model2_df)
+
+  # detect outliers function
+  findoutlier <- function(x) {
+    (x < (quantile(x, .25) - 1.5 * IQR(x))) | 
+      (x > (quantile(x, .75) + 1.5 * IQR(x)))
+  }
+  # create data frame
   plot_df <- model_compare_df %>%
     group_by(model_name) %>%
     mutate(outlier = ifelse(findoutlier(metric), metric, NA))
-   
 
-  if(mode=='classification') {
-    
-  p1 <- ggplot(plot_df, aes(x=model_name, y=metric)) +
-    geom_boxplot() +
-    geom_text(aes(label=outlier), na.rm=TRUE, hjust=-.5) +
-    theme_bw()+
-    labs(y='MCC')
 
-  
+  if (mode == "classification") {
+    p1 <- ggplot(plot_df, aes(x = model_name, y = metric)) +
+      geom_boxplot() +
+      geom_text(aes(label = outlier), na.rm = TRUE, hjust = -.5) +
+      theme_bw() +
+      labs(y = "MCC")
   } else {
-    
-  p1 <- ggplot(plot_df, aes(x=model_name, y=metric)) +
-    geom_boxplot() +
-    geom_text(aes(label=outlier), na.rm=TRUE, hjust=-.5) +
-    theme_bw()+
-    labs(y='RMSE')
+    p1 <- ggplot(plot_df, aes(x = model_name, y = metric)) +
+      geom_boxplot() +
+      geom_text(aes(label = outlier), na.rm = TRUE, hjust = -.5) +
+      theme_bw() +
+      labs(y = "RMSE")
   }
-  
-  #individual taxa 
-  
+
+  # individual taxa
+
   wide_df <- plot_df %>%
-    select(response, model_name, metric, outlier) %>% 
+    select(response, model_name, metric, outlier) %>%
     pivot_wider(names_from = model_name, values_from = metric)
-  
+
   # Calculate differences from the first model (combined)
-  
+
   wide_df <- wide_df %>%
-    mutate( diff_mod1_2 = .[[4]] - .[[3]])
-  
-  
+    mutate(diff_mod1_2 = .[[4]] - .[[3]])
+
+
   # Reshape back to long format for plotting
   long_df <- wide_df %>%
-    pivot_longer(cols = starts_with("diff_"), names_to = "comparison", values_to = "difference")
-  
+    pivot_longer(
+      cols = starts_with("diff_"),
+      names_to = "comparison",
+      values_to = "difference"
+    )
+
   # Plot the differences
-  
-  if(mode=='classification'){
-    
-  p2 <- ggplot(long_df, aes(x=reorder(response, difference, decreasing =T), y=  difference))+
-    geom_bar(stat = "identity") +
-    coord_flip() +
-    labs(x = "Response", y = "Difference in MCC", title = paste(mod_names[2]," vs ",paste(mod_names[1])) ) +
-    theme_bw()
-  
-  } else {
-    
-    p2 <- ggplot(long_df, aes(x=reorder(response, difference, decreasing =T), y=  difference))+
+
+  if (mode == "classification") {
+    p2 <- ggplot(
+      long_df, 
+      aes(
+        x = reorder(response, difference, decreasing = T),
+        y = difference
+      )
+    ) +
       geom_bar(stat = "identity") +
       coord_flip() +
-      labs(x = "Response", y = "Difference in RMSE", title = paste(mod_names[2]," vs ",paste(mod_names[1])) ) +
+      labs(
+        x = "Response",
+        y = "Difference in MCC",
+        title = paste0(mod_names[2], " vs ", paste(mod_names[1]))
+      ) +
       theme_bw()
-    
+  } else {
+    p2 <- ggplot(
+      long_df, 
+      aes(
+        x = reorder(response, difference, decreasing = T),
+        y = difference
+        )
+      ) +
+      geom_bar(stat = "identity") +
+      coord_flip() +
+      labs(
+        x = "Response",
+        y = "Difference in RMSE",
+        title = paste0(mod_names[2], " vs ", paste(mod_names[1]))
+      ) +
+      theme_bw()
   }
 
-  return(list(p1, p2, wide_df))
+  list(
+    p1,
+    p2,
+    wide_df
+  )
 }
