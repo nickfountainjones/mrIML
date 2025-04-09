4e0e4a41e1993995cb046262c47018896f83e9f7
Ryan Leadbetter <ryan.leadbetter@postgrad.curtin.edu.au>
2025-04-09 14:41:19 +1000

    Remove redundancy and tidy up mrPerformancePlot().

--- R/mrPerformancePlot.R
+++ R/mrPerformancePlot.R
@@ -4,7 +4,6 @@
 #'
 #' @param ModelPerf1 Dataframe of model performance metrics for the first model to compare.
 #' @param ModelPerf2 Dataframe of model performance metrics for the second model to compare.
-#' @param mod_names Character vector of model names. Default is \code{c('combined', 'Xonly_model')}.
 #' @param mode Character string indicating whether the mode is 'classification' or 'regression'. Default is 'classification'.
 #' @return A list containing:
 #' \item{p1}{A ggplot object for the boxplot of model performance metrics.}
@@ -16,124 +15,107 @@
 #'
 mrPerformancePlot <- function(ModelPerf1 = NULL,
                               ModelPerf2 = NULL,
-                              mod_names = c("combined", "Xonly_model"),
                               mode = "classification") {
-  m1 <- ModelPerf1[[1]]
-  m2 <- ModelPerf2[[1]]
-
-  d_length <- nrow(m1) # has to be the same
-
-  if (mode == "classification") {
-    m1$mcc <- as.numeric(m1$mcc) # extract mcc
-
-    m1 <- m1 %>%
-      dplyr::rename(metric = mcc)
-
-    model1_df <- m1 %>%
-      tibble(model_type = rep(mod_names[1], d_length)) %>%
-      replace_na(list(metric = 0)) # NAs happen when there is no variance in the model predictions (i.e.all 0s), for these make NAs=0
-
-    m2$mcc <- as.numeric(m2$mcc)
-
-    model2_df <- m2 %>%
-      tibble(model_type = rep(mod_names[2], d_length)) %>%
-      dplyr::rename(metric = mcc) %>%
-      replace_na(list(metric = 0))
-  } else {
-    model1_df <- m1 %>%
-      tibble(model_type = rep(mod_names[1], d_length)) %>%
-      dplyr::rename(metric = rmse)
-
-    model2_df <- m2 %>%
-      tibble(model_type = rep(mod_names[2], d_length)) %>%
-      dplyr::rename(metric = rmse)
+  # Extract performance data frames
+  model1_df <- ModelPerf1[[1]]
+  model2_df <- ModelPerf2[[1]]
+  
+  # Check that the two data frames match 
+  if (!identical(dim(model1_df), dim(model2_df)) |
+      !identical(model1_df$response, model2_df$response)) {
+    stop(
+      "mrIMLperformance objects must be fit to the same data.",
+      call. = FALSE
+    )
   }
-  model_compare_df <- bind_rows(model1_df, model2_df)
-
+  
+  # Get model names for plotting
+  mod_names <- c(
+    model1_df$model_name %>%
+      unique(),
+    model2_df$model_name %>%
+      unique()
+  )
+  
+  # Set the performance metric
+  if (mode == "classification") {
+    metric_name <- "mcc"
+  } else if (mode == "regression") {
+    metric_name <- "rmse"
+  } else {
+    stop("Mode must be either regression or classification.", call. = FALSE)
+  }
+  
   # detect outliers function
   findoutlier <- function(x) {
     (x < (quantile(x, .25) - 1.5 * IQR(x))) | 
       (x > (quantile(x, .75) + 1.5 * IQR(x)))
-  }
-  # create data frame
-  plot_df <- model_compare_df %>%
-    group_by(model_name) %>%
-    mutate(outlier = ifelse(findoutlier(metric), metric, NA))
+  }  
+  
+  model_compare_df <- lapply(
+    list(model1_df, model2_df),
+    function(df) {
+      df %>%
+        dplyr::rename(metric = metric_name) %>%
+        dplyr::mutate(outlier = ifelse(findoutlier(metric), metric, NA))
+    } 
+  ) %>%
+    dplyr::bind_rows()
+  
+  # Create boxplot of model performance metrics
+  p1 <- model_compare_df %>%
+    dplyr::group_by(model_name) %>%
+    ggplot2::ggplot(
+      ggplot2::aes(
+        x = model_name,
+        y = metric,
+        label = round(outlier, 4)
+      )
+    ) +
+    ggplot2::geom_boxplot() +
+    ggplot2::geom_text(
+      na.rm = TRUE, hjust = -0.5
+    ) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(y = toupper(metric_name))
+  
 
-
-  if (mode == "classification") {
-    p1 <- ggplot(plot_df, aes(x = model_name, y = metric)) +
-      geom_boxplot() +
-      geom_text(aes(label = outlier), na.rm = TRUE, hjust = -.5) +
-      theme_bw() +
-      labs(y = "MCC")
-  } else {
-    p1 <- ggplot(plot_df, aes(x = model_name, y = metric)) +
-      geom_boxplot() +
-      geom_text(aes(label = outlier), na.rm = TRUE, hjust = -.5) +
-      theme_bw() +
-      labs(y = "RMSE")
-  }
-
-  # individual taxa
-
-  wide_df <- plot_df %>%
-    select(response, model_name, metric, outlier) %>%
-    pivot_wider(names_from = model_name, values_from = metric)
-
-  # Calculate differences from the first model (combined)
-
-  wide_df <- wide_df %>%
-    mutate(diff_mod1_2 = .[[4]] - .[[3]])
-
+  # Data frame of ndividual taxa
+  wide_df <- model_compare_df %>%
+    dplyr::select(response, model_name, metric, outlier) %>%
+    tidyr::pivot_wider(names_from = model_name, values_from = metric) %>%
+    # Calculate differences from the first model (combined)
+    dplyr::mutate(
+      diff_mod1_2 = .data[[mod_names[2]]] - .data[[mod_names[1]]]
+    )
 
   # Reshape back to long format for plotting
   long_df <- wide_df %>%
-    pivot_longer(
-      cols = starts_with("diff_"),
+    tidyr::pivot_longer(
+      cols = tidyselect::starts_with("diff_"),
       names_to = "comparison",
       values_to = "difference"
     )
 
-  # Plot the differences
-
-  if (mode == "classification") {
-    p2 <- ggplot(
-      long_df, 
-      aes(
-        x = reorder(response, difference, decreasing = T),
-        y = difference
+  # Create bar plot of differences in performance metrics 
+  p2 <- long_df %>%
+    ggplot2::ggplot(
+      ggplot2::aes(
+        y = reorder(response, difference, decreasing = T),
+        x = difference
       )
     ) +
-      geom_bar(stat = "identity") +
-      coord_flip() +
-      labs(
-        x = "Response",
-        y = "Difference in MCC",
-        title = paste0(mod_names[2], " vs ", paste(mod_names[1]))
-      ) +
-      theme_bw()
-  } else {
-    p2 <- ggplot(
-      long_df, 
-      aes(
-        x = reorder(response, difference, decreasing = T),
-        y = difference
-        )
-      ) +
-      geom_bar(stat = "identity") +
-      coord_flip() +
-      labs(
-        x = "Response",
-        y = "Difference in RMSE",
-        title = paste0(mod_names[2], " vs ", paste(mod_names[1]))
-      ) +
-      theme_bw()
-  }
+    ggplot2::geom_bar(stat = "identity") +
+    ggplot2::labs(
+      y = "Response",
+      x = paste0("Difference in ", toupper(metric_name)),
+      title = paste0(mod_names[2], " vs ", mod_names[1])
+    ) +
+    ggplot2::theme_bw()
 
   list(
-    p1,
-    p2,
-    wide_df
+    performance_plot = p1,
+    performance_diff_plot = p2,
+    performance_diff_df = wide_df
   )
 }
