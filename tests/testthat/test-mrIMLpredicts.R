dc414a40f98ed01a01006ee45d3fd913403a1f0b
Ryan Leadbetter <ryan.leadbetter@postgrad.curtin.edu.au>
2025-04-11 16:11:50 +1000

    Add tests for regression models and racing = FALSE + improve error handling for incorect Model argument in mrIMLpredicts().

--- tests/testthat/test-mrIMLpredicts.R
+++ tests/testthat/test-mrIMLpredicts.R
@@ -1,73 +1,160 @@
 # Data for tests
-data <- MRFcov::Bird.parasites
-Y <- data %>%
-  dplyr::select(-scale.prop.zos) %>%
-  dplyr::select(order(tidyselect::everything()))
-X <- data %>%
-  dplyr::select(scale.prop.zos)
-X1 <- Y
+classificaiton_data <- function() {
+  data <- MRFcov::Bird.parasites
+  Y <- data %>%
+    dplyr::select(-scale.prop.zos) %>%
+    dplyr::select(order(tidyselect::everything()))
+  X <- data %>%
+    dplyr::select(scale.prop.zos)
+  X1 <- Y
+  list(
+    Y = Y,
+    X = X,
+    X1 = X1
+  )
+}
+
+regression_data <- function() {
+  data <- iris
+  X <- data[5]
+  Y <- data[-5]
+  X1 <- Y
+  list(
+    Y = Y,
+    X = X,
+    X1 = X1
+  )
+}
+
 # Model for tests
-model_rf <- parsnip::rand_forest(
+# classifier
+model_rf_clas <- parsnip::rand_forest(
   trees = 100,
   mode = "classification",
   mtry = hardhat::tune(),
   min_n = hardhat::tune()
 ) %>%
   parsnip::set_engine("randomForest")
+# regression
+model_rf_reg <- parsnip::rand_forest(
+  trees = 100,
+  mode = "regression",
+  mtry = hardhat::tune(),
+  min_n = hardhat::tune()
+) %>%
+  parsnip::set_engine("randomForest")
 
-mrIMLobj_MR <- mrIMLpredicts(
-  Y = Y,
-  X = X,
-  Model = model_rf,
-  prop = 0.7,
-  k = 5
-)
-
-test_that("both types of model run without error", {
+test_that("both types of model run without error for classification", {
   skip_on_cran()
+  data <- classificaiton_data()
+  # multi-response
   expect_no_error(
     mrIMLpredicts(
-      Y = Y,
-      X = X,
-      Model = model_rf,
+      Y = data$Y,
+      X = data$X,
+      Model = model_rf_clas,
       prop = 0.7,
       k = 5
     )
   )
+  # graphical-network
   expect_no_error(
     mrIMLpredicts(
-      Y = Y,
-      X = X,
-      X1 = X1,
-      Model = model_rf,
+      Y = data$Y,
+      X = data$X,
+      X1 = data$X1,
+      Model = model_rf_clas,
       prop = 0.7,
       k = 5
     )
   )
 })
 
+test_that("both types of model run without error for regression", {
+  skip_on_cran()
+  data <- regression_data()
+  # multi-response
+  expect_no_error(
+    mrIMLpredicts(
+      Y = data$Y,
+      X = data$X,
+      Model = model_rf_reg,
+      prop = 0.7,
+      k = 5
+    )
+  )
+  # graphical-network
+  expect_no_error(
+    mrIMLpredicts(
+      Y = data$Y,
+      X = data$X,
+      X1 = data$X1,
+      Model = model_rf_reg,
+      prop = 0.7,
+      k = 5
+    )
+  )
+})
+
+test_that("works for racing = FALSE", {
+  skip_on_cran()
+  data <- classificaiton_data()
+  expect_no_error(
+    mrIMLpredicts(
+      Y = data$Y,
+      X = data$X,
+      Model = model_rf_clas,
+      prop = 0.7,
+      k = 5,
+      racing = FALSE
+    )
+  )
+})
+  
 test_that("expected dimensions of mrIML obj", {
+  data <- classificaiton_data()
+  mrIMLobj_MR <- mrIMLpredicts(
+    Y = data$Y,
+    X = data$X,
+    Model = model_rf_clas,
+    prop = 0.7,
+    k = 5
+  )
   expect_length(mrIMLobj_MR, 3)
   expect_length(mrIMLobj_MR$Data, 3)
   expect_setequal(
     names(mrIMLobj_MR$Fits),
-    names(Y)
+    names(data$Y)
   )
   expect_s3_class(mrIMLobj_MR$Model, "rand_forest")
   expect_s3_class(mrIMLobj_MR$Model, "model_spec")
 })
 
 test_that("incorect model or data inputs throw errors", {
+  data <- classificaiton_data()
   expect_error(
-    mrIMLpredicts(Y = Y, X = X, Model = lm(Hzosteropis ~ ., cbind(Y, X))),
+    mrIMLpredicts(
+      Y = data$Y, 
+      X = data$X, 
+      Model = lm(Hzosteropis ~ ., cbind(data$Y, data$X))
+    ),
     "The model should be a properly specified tidymodel."
   )
   expect_error(
-    mrIMLpredicts(Y = Y, X = X[1:(nrow(X) - 1), ], Model = model_rf),
+    mrIMLpredicts(
+      Y = data$Y, 
+      X = data$X[1:(nrow(data$X) - 1), ], 
+      Model = model_rf_clas
+    ),
     "All data inputs must have the same number of rows."
   )
   expect_error(
-    mrIMLpredicts(Y = Y, X = X, X1 = X1[1:(nrow(X1) - 1), ], Model = model_rf),
+    mrIMLpredicts(
+      Y = data$Y,
+      X = data$X, 
+      X1 = data$X1[1:(nrow(data$X1) - 1), ],
+      Model = model_rf_clas
+    ),
     "All data inputs must have the same number of rows."
   )
 })
