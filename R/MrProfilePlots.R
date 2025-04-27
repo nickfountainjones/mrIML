f0ae6b82d5edf6db83e82dc5a242c83368bd43cc
Ryan Leadbetter <ryan.leadbetter@postgrad.curtin.edu.au>
2025-04-28 09:34:45 +1000

    Re-work mrProfileplot()

--- R/MrProfilePlots.R
+++ R/MrProfilePlots.R
@@ -1,28 +1,60 @@
-#' mrProfileplot:  Wrapper to plot mutlti-response model agnostic profile plots (partial dependences and accumulated local effects).
-# .
-#' @param profileData \code{dataframe} is the data generated from the flashlight packages 'light_profile' function.
-#' @param 'sdthresh' \code{numeric} value used to filter responses that are not changing across the values of the feature (based on standard deviation)
+#' Wrapper to plot mutlti-response model agnostic profile plots (partial
+#' dependences and accumulated local effects).
+#' 
+#' @param profileData A data frame generated from [flashlight::light_profile()].
+#' @param 'sdthresh' A numeric value used to filter responses that are not
+#' changing across the values of the feature (based on standard deviation).
 #'
-#' @details The aim of this function is to plot (1) a reduced set of response variables that are responding to the feature of choice (plot 1)
-#' and the average ALE of partial dependency  for all responses combined (plot 2). When there are many responses plot 1 makes interpretation easier by focussing on the
-#' the responses changing the most with a feature. The feature selected and plot type must be specified 'light_profile' function
+#' @details The aim of this function is to plot (1) a reduced set of response
+#' variables that are responding to the feature of choice (plot 1) and the
+#' average ALE of partial dependency  for all responses combined (plot 2). When
+#' there are many responses plot 1 makes interpretation easier by focussing on
+#' the responses changing the most with a feature. The feature selected and plot
+#' type must be specified [flashlight::light_profile()] function.
+#' 
 #' @examples
-#' flashlightObj <- mrFlashlight(yhats, X, Y, response = "multi")
+#' library(tidymodels)
+#' library(flashlight)
+#' 
+#' data <- MRFcov::Bird.parasites
+#' Y <- data %>%
+#'   select(-scale.prop.zos) %>%
+#'   select(order(everything()))
+#' X <- data %>%
+#'   select(scale.prop.zos)
+#' 
+#' model_rf <- rand_forest(
+#'   trees = 100, # 100 trees are set for brevity. Aim to start with 1000
+#'   mode = "classification",
+#'   mtry = tune(),
+#'   min_n = tune()
+#' ) %>%
+#'   set_engine("randomForest")
 #'
-#' profileData_pd <- light_profile(flashlightObj, v = "Grassland") # partial dependencies
-#' profileData_ale <- light_profile(flashlightObj, v = "Grassland", type = "ale") # acumulated local effects
-#' mrProfileplot(profileData_pd, sdthresh = 0.05)
-#' mrProfileplot(profileData_ale, sdthresh = 0.05)
+#' mrIML_rf <- mrIMLpredicts(
+#'   X = X,
+#'   Y = Y,
+#'   X1 = Y,
+#'   Model = model_rf,
+#'   prop = 0.7,
+#'   k = 5
+#' )
+#' 
+#' fl <- mrFlashlight(
+#'   mrIML_rf,
+#'   response = "multi"
+#' )
+#'
+#' profileData_pd <- light_profile(fl, v = "scale.prop.zos")
+#' mrProfileplot(profileData_pd, sdthresh = 0.05)[[1]]
+#' mrProfileplot(profileData_pd, sdthresh = 0.05)[[2]]
 #' @export
-
-mrProfileplot <- function(profileData, sdthresh = 0.05) { # from mrFlashlight
-
+mrProfileplot <- function(profileData,
+                          sdthresh = 0.05) {
   b <- profileData$data
-
-  Feature <- names(b[1])
-
+  feature <- names(b[1])
+  
   # select only SNPs that are responding to this featured
-
   std <- b %>%
     dplyr::group_by(label) %>%
     dplyr::summarise(sdALE = sd(value))
@@ -38,14 +70,8 @@ mrProfileplot <- function(profileData, sdthresh = 0.05) { # from mrFlashlight
   redALEplot <- plot(profileData) +
     ggplot2::theme_bw()
 
-  print(redALEplot)
-
-  readline(prompt = " Press [enter] to continue to the global summary plot.")
-
   #-----------------------------------------------
-  # calculate global ALE
-
-  # if(is.factor(b[1])==FALSE){
+  # Calculate global ALE
 
   if (lapply(b[1], is.factor) == FALSE) {
     b[1] <- apply(b[1], 2, as.factor)
@@ -60,7 +86,7 @@ mrProfileplot <- function(profileData, sdthresh = 0.05) { # from mrFlashlight
 
     GlobalPD <- sum %>%
       ggplot2::ggplot(
-        ggplot2::aes_string(Feature, "avgALE")
+        ggplot2::aes_string(feature, "avgALE")
       ) +
       ggplot2::geom_smooth(method = "loess") +
       ggplot2::ylab("Average effect") +
@@ -70,11 +96,15 @@ mrProfileplot <- function(profileData, sdthresh = 0.05) { # from mrFlashlight
   if (lapply(b[1], is.factor) == TRUE) {
     GlobalPD <- b %>%
       ggplot2::ggplot(
-        ggplot2::aes_string(Feature, "value")
+        ggplot2::aes_string(feature, "value")
       ) +
       ggplot2::geom_boxplot(notch = TRUE) +
       ggplot2::theme_bw()
   }
 
-  print(GlobalPD)
+  list(
+    redALEplot,
+    GlobalPD
+  )
+
 }
